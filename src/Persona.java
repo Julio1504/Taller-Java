e26406767ddb2bf731ac28d348ba129b65f9a9d8
Julio <julianorozcol1504@gmail.com>
2024-05-06 16:28:38 -0500

    Encapsulamiento - Implementación de Métodos para Manipulación de Propiedades por [Julian Orozco]

--- src/Persona.java
+++ src/Persona.java
@@ -2,4 +2,28 @@ public class Persona {
     private String nombre;
     private Integer edad;
     private String fechaN;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public String getFechaN() {
+        return fechaN;
+    }
+
+    public void setFechaN(String fechaN) {
+        this.fechaN = fechaN;
+    }
 }
