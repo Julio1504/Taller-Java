6bbc2946e562dde336d02735911662affda4d75b
Julio <julianorozcol1504@gmail.com>
2024-05-06 18:14:40 -0500

    Encapsulamiento y Herencia - Implementación de Métodos para Cálculo de Área y Perímetro por Julian Orozco]

--- src/Circulo.java
+++ src/Circulo.java
@@ -6,4 +6,18 @@ public class Circulo extends FiguraGeometrica{
         this.radio = radio;
     }
 
+    public Integer getRadio() {
+        return radio;
+    }
+
+    public void setRadio(Integer radio) {
+        this.radio = radio;
+    }
+    public void calcularArea(){
+        System.out.println("El area de este circulo es de: \n" + (3.14*this.radio*this.radio) + "cm^2");
+    }
+
+    public void calcularPerimetro(){
+        System.out.println("El perimetro de este rectangulo es de: \n" + (2*3.14*this.radio) + "cm");
+    }
 }
--- src/Rectangulo.java
+++ src/Rectangulo.java
@@ -7,4 +7,27 @@ public class Rectangulo extends FiguraGeometrica{
         this.base = base;
         this.altura = altura;
     }
+
+    public Integer getBase() {
+        return base;
+    }
+
+    public void setBase(Integer base) {
+        this.base = base;
+    }
+
+    public Integer getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Integer altura) {
+        this.altura = altura;
+    }
+    public void calcularArea(){
+        System.out.println("El area de este rectangulo es de: \n" + (this.base*this.altura) + "cm^2");
+    }
+
+    public void calcularPerimetro(){
+        System.out.println("El perimetro de este rectangulo es de: \n" + (this.base*2+this.altura*2) + "cm");
+    }
 }
